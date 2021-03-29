560d641066fadf715e3e0eb8d3fdc78321b66a47
Amilton Santos <amilton@artnew.com.br>
2021-03-29 10:26:52 -0300

    :wrench: Melhora o codigo na api php

--- api-php/api.php
+++ api-php/api.php
@@ -5,18 +5,23 @@
     header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type, Access-Control-Allow-Methods, Authorization, X-Requested-With');
 
     $con = new mysqli('localhost','root', '','nomedobanco');
+    $table = "nometabelabd";
+
+    if ($con -> connect_error){
+        die("Falha na conexão: " .$con->connect_error);
+    }
 
     if($_SERVER['REQUEST_METHOD'] === 'GET'){
         // Pegando as informações do banco de dados
         if(isset($_GET['id'])){
             // Este If é usado, caso de passagem de ID
             $id = $_GET['id'];
-            $sql = $con->query("select * from nometabelabd where id='$id'");
+            $sql = $con->query("SELECT * FROM $table WHERE id='$id'");
             $data = $sql->fetch_assoc();
         }else{
             // Entra nesse, caso não tenha passagem de ID via "get"
             $data = array();
-            $sql = $con->query("select * from nometabelabd");
+            $sql = $con->query("SELECT * FROM $table");
             while($d = $sql->fetch_assoc()){
                 $data[] = $d;
             }
@@ -28,7 +33,7 @@
         if(isset($_GET['id'])){
             $id = $_GET['id'];
             $data = json_decode(file_get_contents("php://input"));
-            $sql = $con->query("update nometabelabd set 
+            $sql = $con->query("UPDATE $table SET 
                 nome = '".$data->nome."', 
                 cidade = '".$data->cidade."', 
                 email = '".$data->email."' 
@@ -44,7 +49,7 @@
     if($_SERVER['REQUEST_METHOD'] === 'POST'){
         // gravar informações
         $data = json_decode(file_get_contents("php://input"));
-        $sql = $con->query("insert into nometabelabd(nome, cidade, email) values ('".$data->nome."','".$data->cidade."','".$data->email."')");
+        $sql = $con->query("INSERT INTO $table (nome, cidade, email) VALUES ('".$data->nome."','".$data->cidade."','".$data->email."')");
         if($sql){
             $data->id = $con->insert_id;
             exit(json_encode($data));
@@ -57,7 +62,7 @@
         // apagar informações do banco
         if(isset($_GET['id'])){
             $id = $_GET['id'];
-            $sql = $con->query("delete from cliente where id='$id'");
+            $sql = $con->query("DELETE FROM $table WHERE id='$id'");
             if($sql){
                 exit(json_encode(array('status' => 'Sucesso')));
             }else{
